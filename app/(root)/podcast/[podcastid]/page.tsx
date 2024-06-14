0d5efbd88c6b4cb674554149865b0355dc2e8c03
Tudish <lokatorist2@inbox.lv>
2024-06-14 21:03:46 +0300

    Created folder structure

---
+++ app/(root)/podcast/[podcastid]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PodcastDetatils = ({ params} : {params : {podcastId: string}}) => {
+  return (
+    <p className='text-white-1'>
+        PodcastDetails for {params.podcastId}
+    </p>
+  )
+}
+
+export default PodcastDetatils
